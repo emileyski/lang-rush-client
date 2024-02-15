87564ed56cafb824eb068daac9d977c174364a08
= <=>
2024-02-15 21:54:54 +0200

    #

--- src/features/words/components/WordsContainer.tsx
+++ src/features/words/components/WordsContainer.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Loader from "src/ui/Loader";
 import { removeTokens } from "src/utils";
 import Word from "./Word";
